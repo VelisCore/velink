c95a12916b460982c2018acd7dcb1d77ebc6f236
wfxey <devinoldenburg16@gmail.com>
2025-07-22 23:43:05 +0200

    Fix dotenv path to load .env from current directory

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 // Load environment variables
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: './.env' });
 
 const express = require('express');
 const cors = require('cors');
